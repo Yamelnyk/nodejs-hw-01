f0ecde1d85348ccca32d41954fb58cf3e83df683
Yamelnyk <yachkana@ukr.net>
2024-09-21 23:51:38 +0300

    done

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,8 @@
-export const removeAllContacts = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const removeAllContacts = async () => {
+  fs.writeFile(PATH_DB, '[]');
+};
 
 removeAllContacts();
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.error('Error while read contacts ', error);
+  }
+};
+
+readContacts();
